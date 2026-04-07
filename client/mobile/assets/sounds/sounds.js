7710ab64d827016ec9c19ffaf593acfa90de5950
89609199136mikle-cpu <89609199136mikle@gmail.com>
2026-04-07 20:57:11 +0700

    Delete client/mobile directory

--- client/mobile/assets/sounds/sounds.js
+++
@@ -1,9 +0,0 @@
-{
-  "notification": "/assets/sounds/notification.mp3",
-  "message": "/assets/sounds/message.mp3",
-  "call": "/assets/sounds/call.mp3",
-  "sent": "/assets/sounds/sent.mp3",
-  "received": "/assets/sounds/received.mp3",
-  "error": "/assets/sounds/error.mp3",
-  "success": "/assets/sounds/success.mp3"
-}
